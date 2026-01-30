7c3939d9060083f9e4b6fd2ac71af4a822d24e90
Keng How <khchin0901@gmail.com>
2026-01-30 21:56:53 +0800

    set up Minnie basic skeleton with greeting and exit

---
+++ src/main/java/Minnie.java
@@ -0,0 +1,19 @@
+public class Minnie {
+
+    public static void main(String[] args) {
+        printGreeting();
+        printGoodbye();
+    }
+
+    public static void printGreeting() {
+        System.out.println("____________________________________________________________________________________________");
+        System.out.println(" Hello! I'm Minnie");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________________________________________");
+    }
+
+    public static void printGoodbye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________________________________________");
+    }
+}
