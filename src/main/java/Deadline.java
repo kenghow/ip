2aa1945621dcdd62d06ff02cd1d4478861b1b8dc
Keng How <khchin0901@gmail.com>
2026-02-05 00:47:18 +0800

    parse deadline dates as LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,21 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    protected LocalDate by;
+
+    public Deadline(String description, LocalDate by) {
         super(description);
         this.by = by;
     }
 
+    public LocalDate getBy() {
+        return by;
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 }
