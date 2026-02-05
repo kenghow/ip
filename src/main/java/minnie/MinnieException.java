8686cee071a288b726983dd25b59e32af0f5ccb0
Keng How <khchin0901@gmail.com>
2026-02-05 20:31:31 +0800

    Reapply "Merge branch 'branch-A-JavaDoc'"
    This reverts commit 6bbdf1e2f9bb09046c890c87b14a787c4c1e161a.

--- src/main/java/minnie/MinnieException.java
+++ src/main/java/minnie/MinnieException.java
@@ -1,5 +1,9 @@
 package minnie;
 
+/**
+ * Represents errors specific to the Minnie chatbot domain,
+ * such as invalid commands or storage format issues.
+ */
 public class MinnieException extends Exception {
     public MinnieException(String message) {
         super(message);
